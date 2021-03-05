a2d872f5e3d1b64a162dc3c2876b79b4cce84f9f
Taylor Otwell <taylorotwell@gmail.com>
2021-03-05 11:18:16 -0600

    wip

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Movie;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Movie::search($request->query('search', 'Star Wars'))->get();
 });
